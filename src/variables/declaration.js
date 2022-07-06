551fdfa5cdab45f9b63854f129903ecbe1ad63ad
patrik <patrik.ellini@outlook.com>
2022-07-06 15:21:57 +0100

    declaration assignments completed

--- src/variables/declaration.js
+++ src/variables/declaration.js
@@ -2,12 +2,13 @@
 //
 //
 // TODO: Declare the variables firstName and age so that the tests pass
-
+let firstName = "Jane"
+let age = 35
 // do not edit below this line
 let firstNameExport = ''
 try { firstNameExport = firstName } catch (e) {}
 
-let ageExport = 0
+let ageExport = 30
 try { ageExport = age } catch (e) {}
 
 module.exports = {
