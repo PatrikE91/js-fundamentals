278b054f01076678b64d8dc1c9f48ee5dfdbce98
patrik <patrik.ellini@outlook.com>
2022-07-12 16:28:08 +0100

    creating functions completed

--- src/functions/creating-functions.js
+++ src/functions/creating-functions.js
@@ -8,6 +8,18 @@
 //
 // TODO: write code below
 
+
+function add(num){
+  num = num + 1
+  return num}
+//  for(let i = 0; i < numOutput; i++){
+//    numOutPut.push(i)
+//  }
+//   return numOutPut
+// }
+// console.log(add(1))
+
+
 // Define a function that takes any person's name and returns it with a smiley :)!
 // Remember to make the name capitalized!
 //
@@ -19,8 +31,30 @@
 //
 // TODO: write code below
 
+
+function sayName(name) {
+  let newName = ''
+    for (i = 0; i < name.length; i++) {
+      if (i === 0) {
+       newName = newName + name[0].toUpperCase()
+      } else {
+          newName = newName + name[i]
+      }
+      // name[0] = name[i].toUpperCase()
+      
+    }
+    
+    
+  return 'Hi, ' + newName + " :)"
+  
+}
+
+console.log(sayName("patrik"))
+
+
+
 // TODO: change undefined to be the name of the functions you defined
 module.exports = {
-  a: undefined, // change undefined to be the name of the function you defined to increment a number (the first TODO)
-  b: undefined // change undefined to be the name of the function you defined to say hi (the second TODO)
+  a: add, // change undefined to be the name of the function you defined to increment a number (the first TODO)
+  b: sayName // change undefined to be the name of the function you defined to say hi (the second TODO)
 }
