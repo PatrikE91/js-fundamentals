a0e08d727f51fd72a7bccc1ae73999e9d7ee7b25
patrik <patrik.ellini@outlook.com>
2022-07-13 16:17:10 +0100

    bool condition test completed

--- src/conditional-flow/boolean-conditions.js
+++ src/conditional-flow/boolean-conditions.js
@@ -1,14 +1,20 @@
 // Initialise the didPass variable with a boolean value
-let didPass
+let didPass = true
 
 // Create a conditional statement that changes the answer variable to the string
 // "Well done, you passed!" if didPass is true, or "Sorry, try again!" if didPass
 // is false
-let answer
+let answer 
 
+if (didPass === true) {
+  answer = 'Well done, you passed!'
+  console.log(answer) 
+} else {
+  answer = 'Sorry, try again!'
+  return answer
+}
 
-
-
+console.log(answer)
 // When you're done and the test passes, changing didPass to the opposite boolean
 // and run the test again to make sure it still passes
 
